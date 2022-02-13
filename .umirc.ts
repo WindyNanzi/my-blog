a9ca18402beb9e01e77bf244c95d5f967df61be2
WindyNanzi <hanqiao1024@163.com>
2022-02-13 14:34:43 +0800

    feat: 添加 faviocn.ico

--- .umirc.ts
+++ .umirc.ts
@@ -3,13 +3,16 @@ import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import navs from './config/navs';
 
-const commonLess = readFileSync(resolve(__dirname, './config/style.less')).toString();
+const commonLess = readFileSync(
+  resolve(__dirname, './config/style.less'),
+).toString();
 
 export default defineConfig({
   title: '东颌子',
   mode: 'site',
   metas: [{ name: 'referrer', content: 'no-referrer' }],
-  styles: [commonLess, ],
-  navs
+  styles: [commonLess],
+  navs,
+  favicon: '/favicon.ico',
   // more config: https://d.umijs.org/config
 });
