ad6ed9009ff06de84fead967f64ef59f783bed5e
hanqiao <hanqiao1024@163.com>
2022-04-29 16:48:57 +0800

    chore: 项目的全新迁移

---
+++ packages/web/src/components/DocNav.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import routes from '~pages'
+
+// eslint-disable-next-line no-console
+console.log(routes)
+</script>
+
+<template>
+  <nav text-xl mt-6 inline-flex gap-2 />
+</template>
---
+++ packages/web/src/pages/docs/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <section>
+    <doc-nav />
+    <router-view />
+  </section>
+</template>
