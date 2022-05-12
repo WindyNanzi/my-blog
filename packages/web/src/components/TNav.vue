4e324c4c13a264a1cbdb4b47fed5b87514892c5d
hanqiao <hanqiao1024@163.com>
2022-05-12 14:37:46 +0800

    fix: 路由切换问题处理

--- packages/web/src/components/TNav.vue
+++ packages/web/src/components/TNav.vue
@@ -1,17 +1,21 @@
 <script setup lang='ts'>
 import { isDark, toggleDark } from '~/composables'
+const router = useRouter()
 
+function go(path: string) {
+  router.push(path)
+}
 </script>
 <template>
   <nav flex justify-between items-center p-6 pt-4 pb-4>
     <div relative>
-      <a href="/" absolute block h-10 w-10 z-2 top--5>
+      <a absolute block h-10 w-10 z-2 top--5 cursor-pointer href="/" @click.prevent="go('/')">
         <img v-if="isDark" src="/NERV-light.svg">
         <img v-else src="/NERV.svg">
       </a>
     </div>
     <div text-5 text-gray-500 flex gap-4>
-      <a class="icon-btn" href="/docs">Blog</a>
+      <a class="icon-btn" href="/docs" @click.prevent="go('/docs')">Blog</a>
 
       <button class="icon-btn !outline-none" @click="toggleDark()">
         <div v-if="isDark" i-carbon-moon />
